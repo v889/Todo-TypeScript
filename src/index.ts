76cf80be3a2d1c9cc056940b727295fc63858d12
v889 <72309826+v889@users.noreply.github.com>
2023-06-26 17:29:20 +0530

    Todo Project

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,61 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+import {v4 as uuidv4} from 'uuid'
 
-import confetti from 'canvas-confetti';
+//console.log(uuidv4())
+const list=document.querySelector<HTMLUListElement>('#list')
+const form=document.querySelector('#task-form') as HTMLFormElement | null
+const input=document.querySelector<HTMLInputElement>('#task-title')
+type Task={
+  id:string
+  title:string
+  isComplete:boolean
+  createdAt:Date
+}
+const tasks:Task[]=loadTasks()
+tasks.forEach(addTask)
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+form?.addEventListener("submit",e=>{
+  console.log(input?.value)
+  e.preventDefault()
+  if(input?.value==='' || input?.value===null) return
+  const task:Task={
+    id:uuidv4(),
+    title:input.value,
+    isComplete:false,
+    createdAt:new Date(),
+
+  }
+  tasks.push(task)
+  saveTasks()
+  addTask(task)
+  input.value=""
+}
+)
+function addTask(task:Task):boolean{
+  const todo=document.createElement('li')
+  const label=document.createElement('label')
+  const checkbox=document.createElement('input')
+  checkbox.type='checkbox'
+  checkbox.addEventListener("change",()=>{
+    task.isComplete=!task.isComplete
+    saveTasks()
+  })
+  label.append(checkbox,task.title)
+  checkbox.checked=task.isComplete
+  todo.append(label)
+  list?.append(todo)
+  return true
+
+}
+function saveTasks()
+{
+  localStorage.setItem("Tasks",JSON.stringify(tasks))
+}
+
+function loadTasks():Task[]{
+  const taskLocal=localStorage.getItem("Tasks")
+  if(taskLocal===null) return []
+  return JSON.parse(taskLocal)
+}
